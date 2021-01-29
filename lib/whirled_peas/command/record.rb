b888f2e7a3b7c3341300326b417641cc8cf2e89b
Tom Collier <collier@apartmentlist.com>
2021-01-28 19:42:17 -0800

    Improve documentation for command line tool

--- lib/whirled_peas/command/record.rb
+++ lib/whirled_peas/command/record.rb
@@ -10,6 +10,28 @@ module WhirledPeas
       def start
         raise NotImplementedError
       end
+
+      private
+
+      attr_reader :out_file
+
+      def validate!
+        super
+        return unless @error_text.nil?
+
+        out_file = args.shift
+        if out_file.nil?
+          @error_text = "#{command_name} requires an output file"
+        elsif !out_file.end_with?('.fgz')
+          @error_text = "Expecting output file with .fgz extension, found: .#{out_file.split('.').last}"
+        else
+          @out_file = out_file[0] == '/' ? out_file : File.join(Dir.pwd, out_file)
+        end
+      end
+
+      def options_usage
+        [*super, '<output file>'].join(' ')
+      end
     end
   end
 end
