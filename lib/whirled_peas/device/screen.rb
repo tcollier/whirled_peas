75ffc27c3224f82ada163a84bd428eed7cc71d4f
Tom Collier <collier@apartmentlist.com>
2021-02-03 20:17:27 -0800

    Simplify next_frame_at

--- lib/whirled_peas/device/screen.rb
+++ lib/whirled_peas/device/screen.rb
@@ -8,14 +8,9 @@ module WhirledPeas
       end
 
       def handle_rendered_frames(rendered_frames)
-        next_frame_at = nil
+        next_frame_at = Time.now
         rendered_frames.each do |rendered_frame|
-          if next_frame_at.nil?
-            next_frame_at = Time.now + rendered_frame.duration
-          else
-            next_frame_at += rendered_frame.duration
-            next if next_frame_at < Time.now
-          end
+          next_frame_at += rendered_frame.duration
           output.print(rendered_frame.strokes)
           output.flush
           sleep([0, next_frame_at - Time.now].max)
